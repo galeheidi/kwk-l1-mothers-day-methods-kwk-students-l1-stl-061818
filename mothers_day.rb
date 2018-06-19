289de3ad23c46b037990b502d3e09c300c819b26
galeheidi <kgaletax@aol.com>
2018-06-19 15:54:29 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,7 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day (time_of_day,name)
+ puts "Happy #{time_of_day} #{name}!" 
+end 
+mothers_day ("mothers_day","mom")
 
